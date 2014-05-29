8c65cd5279ddfeec2e36a307a8ab7b4897ae6da8
no-one <no-one@fb.com>
2014-05-28 21:27:43 -0700

    Animations theme

--- wp-content/themes/phusis/animations/header.php
+++ wp-content/themes/phusis/animations/header.php
@@ -46,7 +46,7 @@
 
 </head>
 
-<body>
+<body class="animations">
 <div id="wrapper">
 	<div class="search" id="<?php if (rand(0, 1) == 0) { echo 'search-phusis'; } else { echo 'search-cygne'; } ?>">
     <h1><img alt="<?php bloginfo('description'); ?>" width="520" height="42" src="<?php bloginfo('template_directory'); ?>/images/phusis_phrase_michel.png" /></h1>
@@ -58,79 +58,61 @@
 
     <!-- New menu -->
     <div id="highlight-menu">
-      <!-- Zarathustra -->
       <?php
-      $article_list_template = '<li><a href="{url}"><span class="date">{date:d.m.}</span> <span class="title">{title:18:trim}</span></a></li>';
+      /*$article_list_template = '<li><a href="{url}"><span class="date">{date:d.m.}</span> <span class="title">{title:18:trim}</span></a></li>';
       $highlight_image_template = '<a href="{url}"><img alt="{imagealt}" src="{imagesrc}" height="72" width="64" /></a>';
 
-      $recent_posts_params='limit=3&prefix=<ul class="derniers-articles">&suffix=</ul>&output_template='.$article_list_template; ?>
+      $recent_posts_params='limit=3&prefix=<ul class="derniers-articles">&suffix=</ul>&output_template='.$article_list_template;*/ ?>
 
       <div class="highlight-menu-item">
       <div>
-      <h3><a href="http://phusis.ch/michel/philosophie/nietzsche/traductions/zarathoustra/">Zarathoustra</a></h3>
-      <p><?php if (function_exists('recent_posts')) recent_posts('included_cats=24&limit=1&prefix=&suffix=&output_template='.$highlight_image_template); ?></p>
-      <?php if (function_exists('recent_posts')) recent_posts('included_cats=24&'.$recent_posts_params); ?>
+      <h3><a href="<?php echo get_category_link(get_category_by_slug('sport'));?>">Sport</a></h3>
+      <p><img src="<?php bloginfo('template_directory'); ?>/images/animations/menu_sport.jpg" /></p>
       </div>
       </div>
 
-      <!-- Les Bacchantes -->
-      <?php //if (function_exists('recent_posts')) recent_posts('included_cats=28&' . $recent_posts_params); ?>
       <div class="highlight-menu-item">
       <div>
-      <h3><a href="http://phusis.ch/michel/dionysos/les-bacchantes/">Les Bacchantes</a></h3>
-      <p><?php if (function_exists('recent_posts')) recent_posts('included_cats=28&limit=1&prefix=&suffix=&output_template='.$highlight_image_template); ?></p>
-      <?php if (function_exists('recent_posts')) recent_posts('included_cats=28&'.$recent_posts_params); ?>
+      <h3><a href="<?php echo get_category_link(get_category_by_slug('communication'));?>">Communication</a></h3>
+      <p><img src="<?php bloginfo('template_directory'); ?>/images/animations/menu_communication.jpg" /></p>
       </div>
       </div>
 
-      <!-- Ludo et Bboul
-      <h3>Ludo et Bboul</h3>-->
-      <?php //if (function_exists('recent_posts')) recent_posts('included_cats=30&' . $recent_posts_params); ?>
       <div class="highlight-menu-item">
       <div>
-      <h3><a href="http://phusis.ch/michel/pensees-phusiques/">Chroniques</a></h3>
-      <p><?php if (function_exists('recent_posts')) recent_posts('included_cats=13&limit=1&prefix=&suffix=&output_template='.$highlight_image_template); ?></p>
-      <?php if (function_exists('recent_posts')) recent_posts('included_cats=13&'.$recent_posts_params); ?>
+      <h3><a href="<?php echo get_category_link(get_category_by_slug('traductions'));?>">Traductions</a></h3>
+      <p><img src="<?php bloginfo('template_directory'); ?>/images/animations/menu_traductions.jpg" /></p>
       </div>
       </div>
 
-      <!-- Animation
-      <h3>Animation</h3>-->
-      <?php //if (function_exists('recent_posts')) recent_posts('included_cats=6&' . $recent_posts_params); ?>
       <div class="highlight-menu-item">
       <div>
-      <h3><a href="http://phusis.ch/michel/animations-contact/">Animations</a></h3>
-      <p><?php if (function_exists('recent_posts')) recent_posts('included_cats=6&limit=1&prefix=&suffix=&output_template='.$highlight_image_template); ?></p>
-      <?php if (function_exists('recent_posts')) recent_posts('included_cats=6&'.$recent_posts_params); ?>
+      <h3><a href="<?php echo get_category_link(get_category_by_slug('conferences'));?>">Conférences</a></h3>
+      <p><img src="<?php bloginfo('template_directory'); ?>/images/animations/menu_conferences.jpg" /></p>
       </div>
       </div>
 
-      <!-- Films de la semaine
-      <h3>Films de la semaine</h3>-->
-      <?php //if (function_exists('recent_posts')) recent_posts('included_cats=31&' . $recent_posts_params); ?>
       <div style="clear:none"></div>
     </div>
 
 		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
-    <?php if (is_page('Bienvenue')) { ?>
-      <p id="featuredposts"><img alt="&#x1f4fa;" src="<?php bloginfo('template_directory'); ?>/images/television.png" height="32" class="TV" /> <?php get_featured_posts(array('before' => '', 'after' => '', 'max_posts' => 1, 'method' => 'echo')); ?> </p>
-    <?php } ?>
 	</div>
 
 	<div id="main_nav">
 		<h1 class="masthead"><a href="<?php bloginfo('url');?>/biographie/"><?php bloginfo('name'); ?></a></h1>
 
 		<ul>
-	    <li class="otherblog"><a href="/phusis/">PHUSIS</a></li>
 			<?php //wp_list_pages('title_li=&depth=1'); ?>
       <?php $output = wp_list_pages('echo=0&depth=1&title_li=' );
       echo $output;
       ?>
+      <li class="otherblog"><a href="/phusis/">PHUSIS</a></li>
+      <li class="otherblog"><a href="/steve/">PHUSIS Philosophie</a></li>
 	    <li class="otherblog"><a href="/steve/">PHUSIS Vins</a></li>
 		</ul>
 
     <p class="video">
-      <?php if (function_exists('recent_posts')) recent_posts('included_cats=12&limit=3&prefix=&suffix=&output_template=<a href="{url}"><span class="title">{title}</span><span class="date">{date}</span><span class="preview"><img src="{videopreviewsrc}" width="64" height="48" alt="Aperçu : {title}" /><span class="flash-icon" ></span></span></a>'); ?>
+      <?php if (function_exists('recent_posts')) recent_posts('included_cats=5&limit=3&prefix=&suffix=&output_template=<a href="{url}"><span class="title">{title}</span><span class="date">{date}</span><span class="preview"><img src="{videopreviewsrc}" width="64" height="48" alt="Aperçu : {title}" /><span class="flash-icon" ></span></span></a>'); ?>
     </p>
 
 	</div>
